cfc07d3434262dace1cf9c673790b8600d1866cc
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-07-04 08:40:07 -0300

    feat: :sparkles: create hook useRequest

--- src/components/hooks/useRequest.ts
+++ src/components/hooks/useRequest.ts
@@ -4,10 +4,14 @@ import { useEffect, useState } from "react";
 export function useRequest<T = unknown>(url: string) {
   const [data, setData] = useState<T | null>(null);
 
-  useEffect(() => {
-    axios.get(url).then((res) => {
-      setData(res.data.products);
-    });
-  }, []);
+  try {
+    useEffect(() => {
+      axios.get(url).then((res) => {
+        setData(res.data.products);
+      });
+    }, []);
+  } catch (error) {
+    console.log(error);
+  }
   return { data, setData };
 }
