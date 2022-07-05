04bb0328af08776199f8cc985a8b7c63041b7b29
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-07-05 09:18:16 -0300

    Update Router.tsx

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Error } from "../pages/Error";
 import { Home } from "../pages/Home";
-import { Product } from "../pages/Product";
+// import { Product } from "../pages/Product";
 import { Wishlist } from "../pages/Wishlist";
 
 export function Router() {
@@ -10,7 +10,7 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="products" element={<Home />} />
       <Route path="wishlist" element={<Wishlist />} />
-      <Route path="products/:sku" element={<Product />} />
+      {/* <Route path="products/:sku" element={<Product />} /> */}
       <Route path="/*" element={<Error />} />
     </Routes>
   );
