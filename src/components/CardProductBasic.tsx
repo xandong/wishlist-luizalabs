8bd490ecb5d7da4be7e03ba8087d23ebda88905a
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-07-05 09:09:03 -0300

    Update CardProductBasic.tsx

--- src/components/CardProductBasic.tsx
+++ src/components/CardProductBasic.tsx
@@ -64,7 +64,7 @@ export function CardProductBasic({
       )}
       <Link
         onClick={saveFavorites}
-        // to={`products/${sku}`}
+        // to={`products/${sku}`} // Ao terminar pagina de produto, mudar para esse redirecionamento
         to={`/products`}
         className="w-48 min-h-[18rem] p-2 mb-4
         flex flex-col items-center gap-4
